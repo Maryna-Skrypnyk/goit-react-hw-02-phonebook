f83a4cc04fb1a9796526e76461c7e3e4efa0563d
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2021-07-11 18:51:15 +0300

    Adds base

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const ContactList = ({ contacts }) => {
+  return <ul>{contacts.map(ContactItem)}</ul>;
+};
+
+const ContactItem = ({ id, name }) => {
+  return <li key={id}>{name}</li>;
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default ContactList;
